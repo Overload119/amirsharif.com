6a00fc1fbdf530972d8dfac9d5d283d354c3ed76
Amir Sharif <budgeneration@gmail.com>
2012-07-30 21:57:55 -0400

    Add Heroku things

---
+++ web.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/img/*', function (req, res) {
+	var resource_path = __dirname + '/img/' + req.params[0];
+	res.sendfile(resource_path);
+});
+
+app.get('/css/*', function (req, res) {
+	var resource_path = __dirname + '/css/' + req.params[0];
+	res.sendfile(resource_path);
+});
+
+app.get('/js/*', function (req, res) {
+	var resource_path = __dirname + '/js/' + req.params[0];
+	res.sendfile(resource_path);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
