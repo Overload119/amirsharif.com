c8d4c514f3e668d4286c6a90495e307ef059e555
Amir Sharif <budgeneration@gmail.com>
2012-07-31 21:17:41 -0400

    Fix spacing

--- js/home.js
+++ js/home.js
@@ -5,36 +5,50 @@ $.fx.step.textShadowBlur = function(fx) {
 var things = ['building web applications', 'designing interfaces', 'creating mockups', 'watching movies', 'testing methods', 'eating cereal', 'refactoring', 'reading', 'proving theorems', 'kissing girls', 'building systems', 'playing StarCraft 2', 'Redditing', 'learning'];
 var things_index = 0;
 
+function GetTextWidth(text) {
+  $('#spacer').html(text);
+  var width = $('#spacer').width();
+  return width;
+}
+
 function Shift() {
   if(things_index >= things.length-1) {
     things_index = 0;
   } else {
     things_index++;
   }  
-
+  var next_thing = things[things_index];
+  
+  // Get the width we have to change to accomodate the new thing
+  var _temp = $('#shift').html();
+  $('#shift').html(next_thing);
+  var width = GetTextWidth($('#shift').parent().text());
+  $('#shift').html(_temp);
+  
   $('#shift').css({
     'textShadowBlur': 1,
     'opacity': 1
   });
   
+  // Fade out
   $('#shift').animate({
-    'textShadowBlur': 40,
-    'opacity': 0
-  }, 500, function() {    
-    $('#shift').html(things[things_index]);
+      'textShadowBlur': 40,
+      'opacity': 0
+    }, 500, function() {    
+    // Adjust size
+    $('#shift').parent().animate({
+      'width': width + 'px'
+    }, 200, function() {
+      // Fade In
+      $('#shift').html(next_thing);
     
-    $('#spacer').html($('#shift').parent().text());  
-    var new_width = $('#spacer').width();
-    
-    $('#shift').parent().css({
-      'width': new_width + 'px'
+      $('#shift').animate({
+        'textShadowBlur': 0,
+        'opacity': 1
+      }, 500);
     });  
-  
-    $('#shift').animate({
-      'textShadowBlur': 0,
-      'opacity': 1
-    }, 500);
-  });
+  });  
+
   setTimeout(Shift, 3000);
 }
 
