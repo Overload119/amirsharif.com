2b0a5c7bf2a3c16950083976291fdb27d79d875e
overload119 <budgeneration@gmail.com>
2012-11-24 10:38:00 -0500

    Add the beginnings of the Movember page

---
+++ js/mustache.js
@@ -0,0 +1,15 @@
+(function() {
+
+  // Preload the image
+  var canvas    = document.getElementById( 'mustache' );
+  var c         = c.getContext( '2d' );
+
+  var imageOne  = new Image();
+  imageOne.src  = '';
+  var imageTwo  = new Image();
+  imageTwo.src  = '';
+
+  
+
+
+})();
