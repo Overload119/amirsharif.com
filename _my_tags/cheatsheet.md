6bc0bd51de7081d3e71306411aade45d9b06173b
Amir Sharif <budgeneration@gmail.com>
2018-01-08 21:09:04 -0800

    Attempt at refactoring to new layout [ci skip]

--- _my_tags/cheatsheet.md
+++
@@ -1,4 +0,0 @@
----
-slug: cheatsheet
-name: Cheatsheet
----
---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "My Top Apps, Routines, and Products",
+        "excerpt":"Apps Alfred - Award-winning productivity app Dropbox - A folder that syncs Spotify - Music BitBar - Put anything in your Mac OS X menu bar Caret - Beautiful &amp; Clever Markdown Editor Kaleidoscope - Powerful file comparison tool (works on text, images, and folders) Spectacle - Move and resize...","categories": [],
+        "tags": [],
+        "url": "https://amirsharif.com/top-routines-apps-and-products/",
+        "teaser":null}]
